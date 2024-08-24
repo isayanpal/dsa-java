67e552b6337a633b1601e2bbfdb3c3834b8af11a
Sayan Snigdha Pal <sayanpalb@gmail.com>
2024-08-24 12:30:42 +0530

    medium level arrays and strings

---
+++ Leetcode_qs/RotateImage.java
@@ -0,0 +1,27 @@
+package Leetcode_qs;
+
+//LC48
+
+public class RotateImage {
+    public void solution(int[][] matrix){
+        int n = matrix.length;
+
+        //Transpose
+        for (int i=0; i<n; i++){
+            for (int j = i+1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        //Reflection
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
+    }
+}
