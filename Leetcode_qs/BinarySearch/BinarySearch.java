4a3a99da8cd15aa0eee05b672d10ac943fcf835b
Sayan Snigdha Pal <sayanpalb@gmail.com>
2024-09-16 13:35:47 +0530

    binary search qs

---
+++ Leetcode_qs/BinarySearch/BinarySearch.java
@@ -0,0 +1,23 @@
+package Leetcode_qs.BinarySearch;
+
+//LC704
+
+public class BinarySearch {
+    public int search(int[] nums, int target){
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right){
+            int middle = left + ((right-left) / 2);
+
+            if (nums[middle] == target){
+                return middle;
+            } else if (nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ Leetcode_qs/BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,22 @@
+package Leetcode_qs.BinarySearch;
+
+//LC35
+
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target){
+        int left = 0, right = nums.length - 1;
+
+        while(left <= right){
+            int mid = left + ((right-left) / 2);
+
+            if (nums[mid] < target){
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return left;
+    }
+}
