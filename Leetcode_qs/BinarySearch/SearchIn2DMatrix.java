1e21ec2580e92c2cee594d2e8d7972a2d1743e2d
Sayan Snigdha Pal <sayanpalb@gmail.com>
2024-09-19 12:15:11 +0530

    binary search qs

---
+++ Leetcode_qs/BinarySearch/SearchIn2DMatrix.java
@@ -0,0 +1,28 @@
+package Leetcode_qs.BinarySearch;
+
+//LC74
+
+public class SearchIn2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target){
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int l = 0;
+        int r = m*n-1;
+
+        while (l<=r){
+            int mid = (l+r) / 2;
+            int i = mid / n;
+            int j = mid % n;
+            int midNum = matrix[i][j];
+
+            if (target == midNum){
+                return true;
+            } else if (target < midNum) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return false;
+    }
+}
