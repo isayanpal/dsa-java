dd8c077dc40af94d2e62303fd4fd1424633b2ba9
Sayan Snigdha Pal <sayanpalb@gmail.com>
2024-09-25 19:17:21 +0530

    trees qs

---
+++ Leetcode_qs/Trees/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,30 @@
+package Leetcode_qs.Trees;
+
+//LC104
+
+public class MaximumDepthOfBinaryTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+        TreeNode(int val){
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public int maxDepth(TreeNode root){
+        if (root == null){
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return 1 + Math.max(left,right);
+    }
+}
