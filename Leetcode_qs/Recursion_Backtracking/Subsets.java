3912974610ee6dbdbbb08df2ab96f88ae513a463
Sayan Snigdha Pal <sayanpalb@gmail.com>
2024-10-07 22:20:39 +0530

    recursion backtracking

---
+++ Leetcode_qs/Recursion_Backtracking/Subsets.java
@@ -0,0 +1,24 @@
+package Leetcode_qs.Recursion_Backtracking;
+
+//LC78
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+    public List<List<Integer>> subsets(int[] nums){
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> sol = new ArrayList<>();
+        backtrack(nums, 0, sol,res);
+        return res;
+    }
+
+    private void backtrack(int[] nums, int start, List<Integer> sol, List<List<Integer>> res){
+        res.add(new ArrayList<>(sol));
+        for (int i = start; i < nums.length; i++) {
+            sol.add(nums[i]);
+            backtrack(nums,i+1, sol, res);
+            sol.remove(sol.size() - 1);
+        }
+    }
+}
