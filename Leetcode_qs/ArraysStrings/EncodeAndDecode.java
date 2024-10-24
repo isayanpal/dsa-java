7ff6a5657473b15400ded306cead32ff7578a1b7
Sayan Snigdha Pal <sayanpalb@gmail.com>
2024-10-24 20:20:30 +0530

    array qs

---
+++ Leetcode_qs/ArraysStrings/EncodeAndDecode.java
@@ -0,0 +1,27 @@
+package Leetcode_qs.ArraysStrings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EncodeAndDecode {
+    public static String encode(List<String> strs){
+        StringBuilder encodedString = new StringBuilder();
+        for (String str : strs){
+            encodedString.append(str.length()).append("#").append(str);
+        }
+        return encodedString.toString();
+    }
+    public static List<String> decode(String str){
+        List<String> list = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()){
+            int j = i;
+            while (str.charAt(j) != '#') j++;
+
+            int length = Integer.valueOf(str.substring(i,j));
+            i = j + 1 + length;
+            list.add(str.substring(j + 1, i));
+        }
+        return list;
+    }
+}
